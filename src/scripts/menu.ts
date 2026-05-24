b35ed4fb6ecf310f0caf0adb656d38b5fdfd5080
leslylulu <dearlulu1995@gmail.com>
2026-05-24 19:36:32 -0400

    fix: menu ts types check

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,6 +1,7 @@
-const menu = document.querySelector('.menu');
+const menu = document.querySelector<HTMLButtonElement>('.menu');
 
 menu?.addEventListener('click', () => {
+	if(!menu) return;
 	const isExpanded = menu.getAttribute('aria-expanded') === 'true';
 	menu.setAttribute('aria-expanded', `${!isExpanded}`);
 });
